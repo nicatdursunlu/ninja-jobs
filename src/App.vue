43e0ebc03e74a6b11edc42926e1002e2d4bbca7a
Nicat DUrsunlu <nicat.dursunlu@gmail.com>
2021-03-09 14:32:55 +0400

    finish project

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link>
     <router-link :to="{ name: 'About' }">About</router-link>
     <router-link :to="{ name: 'Jobs' }">Jobs</router-link>
   </div>
+
+  <button @click="redirect">Redirect</button>
+  <button @click="back">Go back</button>
+  <button @click="forward">Go forward</button>
+
   <router-view />
 </template>
+
+<script>
+export default {
+  methods: {
+    redirect() {
+      this.$router.push({ name: "Home" });
+    },
+    back() {
+      this.$router.go(-1);
+    },
+    forward() {
+      this.$router.go(1);
+    },
+  },
+};
+</script>
+
 
 <style>
 #app {
@@ -31,5 +53,14 @@
 #nav a.router-link-exact-active {
   color: white;
   background: crimson;
+}
+
+button {
+  margin: 0 10px;
+  padding: 10px;
+  border-radius: 4px;
+  border: none;
+  outline: none;
+  cursor: pointer;
 }
 </style>
